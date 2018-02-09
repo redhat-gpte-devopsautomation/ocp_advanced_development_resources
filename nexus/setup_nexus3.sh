b3b85a3513ef9af33e4dcc0e4fbf9fbf2ff3661c
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-02-09 15:30:10 -0500

    Added private docker registry

--- nexus/setup_nexus3.sh
+++ nexus/setup_nexus3.sh
@@ -104,15 +104,45 @@ EOM
   curl -v -X POST -H "Content-Type: text/plain" -u "$_NEXUS_USER:$_NEXUS_PWD" "${_NEXUS_URL}/service/rest/v1/script/$_GROUP_ID/run"
 }
 
+#
+# Add a Docker Registry Repo to Nexus3
+# add_nexus3_docker_repo [repo-id] [repo-port] [nexus-username] [nexus-password] [nexus-url]
+#
+function add_nexus3_docker_repo() {
+  local _REPO_ID=$1
+  local _REPO_PORT=$2
+  local _NEXUS_USER=$3
+  local _NEXUS_PWD=$4
+  local _NEXUS_URL=$5
+
+  read -r -d '' _REPO_JSON << EOM
+{
+  "name": "$_REPO_ID",
+  "type": "groovy",
+  "content": "repository.createDockerHosted('$_REPO_ID',$_REPO_PORT,null)"
+}
+EOM
+
+  curl -v -H "Accept: application/json" -H "Content-Type: application/json" -d "$_REPO_JSON" -u "$_NEXUS_USER:$_NEXUS_PWD" "${_NEXUS_URL}/service/rest/v1/script/"
+  curl -v -X POST -H "Content-Type: text/plain" -u "$_NEXUS_USER:$_NEXUS_PWD" "${_NEXUS_URL}/service/rest/v1/script/$_REPO_ID/run"
+}
+
 # Main body of the script
 # $1: Nexus3 User ID
 # $2: Nexus3 Password
 # $3: Nexus3 URL
 
+# Red Hat Proxy Repos
 add_nexus3_proxy_repo redhat-ga https://maven.repository.redhat.com/ga/ $1 $2 $3
 add_nexus3_proxy_repo jboss https://repository.jboss.org/nexus/content/groups/public/ $1 $2 $3
 
+# Repo Group to include all proxy repos
 add_nexus3_group_proxy_repo redhat-ga,jboss,maven-central,maven-releases,maven-snapshots maven-all-public $1 $2 $3
 
-add_nexus3_release_repo releases $1 $2 $3
+# NPM Proxy Repo
 add_nexus3_npmproxy_repo npm https://registry.npmjs.org/ $1 $2 $3
+# Private Docker Registry
+add_nexus3_docker_repo docker 5000 $1 $2 $3
+
+# Maven release Repo
+add_nexus3_release_repo releases $1 $2 $3
